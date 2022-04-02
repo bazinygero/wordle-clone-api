8121c334fb51edf6da9675cc7b601b1e701d2b2c
bazinygero <97780599+bazinygero@users.noreply.github.com>
2022-04-02 22:37:53 +0300

    done for today, stuck

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const tileDisplay = document.querySelector('.tile-container')
 const keyboard = document.querySelector('.key-container')
 
+const wordle = 'SUPER'
+
 const keys = [
     'Q',
     'W',
@@ -32,6 +34,9 @@ const keys = [
     '<<',
 ]
 
+let currentRow = 0
+let currentTile = 0
+
 const guessRows = [
     ['', '', '', '', ''],
     ['', '', '', '', ''],
@@ -53,14 +58,35 @@ guessRows.forEach((guessRow, guessRowIndex) => {
     tileDisplay.append(rowElement)
 })
 
-const handleClick = () => {
-    console.log('clicked')
-}
+
 
 keys.forEach(key => {
     const buttonElement = document.createElement('button')
     buttonElement.textContent = key
     buttonElement.setAttribute('id', key)
-    buttonElement.addEventListener('click', handleClick)
+    buttonElement.addEventListener('click', () => handleClick(key))
     keyboard.append(buttonElement)
 })
+
+const handleClick = (letter) => {
+    console.log('clicked', letter)
+    if (letter === '<<') {
+        console.log('delete letter')
+        return
+    }
+    if (letter === 'ENTER')
+        console.log('check row')
+        return
+    addLetter(letter)
+}
+
+const addLetter = (letter) => {
+    if (currentTile < 5 && currentRow < 6) {
+        const tile = document.getElementById('guessRow-' + currentRow + '-tile-' + currentTile)
+        tile.textContent = letter
+        guessRows[currentRow][currentTile] = letter
+        tile.setAttribute('data', letter)
+        currentTile++
+        console.log('guessRows', guessRows)
+    }
+}
